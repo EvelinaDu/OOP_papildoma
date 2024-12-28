a36cb69e16d3c9e2ef9e43c1f35a8594ad1b8719
EvelinaDu <evelinadudutyte9@gmail.com>
2024-12-28 04:22:16 +0200

    Perdaryta kodo struktūra, pridėta funkcija, kuri pakeičią, atnaujiną žodį.

--- src/text_analysis.cpp
+++ src/text_analysis.cpp
@@ -1,9 +1,20 @@
 #include "../include/Mylib.h"
 
+string pakeistas_zodis(const string& zodis) {
+    string naujas_zodis;
+    for (char ch : zodis) {
+        ch = tolower(ch);
+        if (isalnum(ch)) {
+            naujas_zodis += ch;
+        }
+    }
+    return naujas_zodis;
+}
+
 void failo_tvarkymas(){
     string failo_pavadinimas;
     std::ifstream failas;
-    // string eil;
+    std::map<string, int> zodziu_kiekis;
 
     cout << "Įveskite failo pavadinimą: ";
     
@@ -23,15 +34,10 @@ void failo_tvarkymas(){
                 string zodis;
 
                 while(strstream >> zodis){
-                    string naujas_zodis;
-                    for (char ch : zodis) {
-                        if(isalnum(ch)){
-                            naujas_zodis += ch;
-                        }
-                    }
+                    string naujas_zodis = pakeistas_zodis(zodis);
+                    zodziu_kiekis[naujas_zodis]++;
                     cout << naujas_zodis << endl;
                 }
-
             }
             
             failas.close();
@@ -41,4 +47,6 @@ void failo_tvarkymas(){
         }
 
     }
+
+
 }
